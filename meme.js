1d9eabc665e92d7d8f264f5436973b3a20eaacca
Olarewaju <maqsoud.tawaliou@epitech.eu>
2024-04-17 16:36:00 +0100

    Adding the meme file

---
+++ meme.js
@@ -0,0 +1,94 @@
+const imageFileInput = document.querySelector("#imageFileInput");
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
+const textColorInput = document.querySelector("#textColorInput");
+const textBorderColorInput = document.querySelector("#textBorderColorInput");
+const canvas = document.querySelector("#meme");
+const downloadButton = document.querySelector("#downloadButton");
+
+let image;
+
+imageFileInput.addEventListener("change", () => {
+  const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+
+  image = new Image();
+  image.src = imageDataUrl;
+
+  image.addEventListener("load", () => {
+    updateMemeCanvas(
+      canvas,
+      image,
+      topTextInput.value,
+      bottomTextInput.value,
+      textColorInput.value,
+      textBorderColorInput.value
+    );
+  });
+});
+
+topTextInput.addEventListener("input", () => {
+  console.log(topTextInput.value);
+  updateMemeCanvas(
+    canvas,
+    image,
+    topTextInput.value,
+    bottomTextInput.value,
+    textColorInput.value,
+    textBorderColorInput.value
+  );
+});
+
+bottomTextInput.addEventListener("input", () => {
+  updateMemeCanvas(
+    canvas,
+    image,
+    topTextInput.value,
+    bottomTextInput.value,
+    textColorInput.value,
+    textBorderColorInput.value
+  );
+});
+
+downloadButton.addEventListener("click", () => {
+  const dataUrl = canvas.toDataURL("image/jpeg");
+  const a = document.createElement("a");
+  a.href = dataUrl;
+  a.download = "meme.jpg";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+function updateMemeCanvas(
+  canvas,
+  image,
+  topText,
+  bottomText,
+  textColor,
+  textBorderColor
+) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);
+
+  ctx.strokeStyle = textBorderColor;
+  ctx.lineWidth = Math.floor(fontSize / 4);
+  ctx.fillStyle = textColor;
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-serif`;
+
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
+}
